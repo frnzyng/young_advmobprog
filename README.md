28a9e09985914f1f858bdfdcc8032a209cee9008
frnzyng <franzeangelei@gmail.com>
2025-08-27 20:19:47 +0800

    Initial commmit

---
+++ README.md
@@ -0,0 +1,3 @@
+# young_advmobprog
+
+A new Flutter project.
---
+++ lib/main.dart
@@ -0,0 +1,46 @@
+import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
+import 'package:flutter_dotenv/flutter_dotenv.dart';
+import 'package:flutter_screenutil/flutter_screenutil.dart';
+import 'package:provider/provider.dart';
+import 'package:young_advmobprog/providers/theme_provider.dart';
+import 'package:young_advmobprog/screens/home_screen.dart';
+
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await dotenv.load(fileName: 'assets/.env'); 
+  SystemChrome.setPreferredOrientations([DeviceOrientation.portraitUp])
+      .then((_) {
+    runApp(const MainApp());
+  });
+}
+
+class MainApp extends StatelessWidget {
+  const MainApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return ChangeNotifierProvider(
+      create: (_) => ThemeProvider(),
+      child: ScreenUtilInit(
+        designSize: const Size(412, 715),
+        minTextAdapt: true,
+        splitScreenMode: true,
+        builder: (context, child) {
+          final themeModel = context.watch<ThemeProvider>();
+          return MaterialApp(
+            debugShowCheckedModeBanner: false,
+            theme: ThemeData.light(),
+            darkTheme: ThemeData.dark(),
+            themeMode: themeModel.isDark ? ThemeMode.dark : ThemeMode.light,
+            title: 'Blog App',
+            initialRoute: '/home',
+            routes: {
+              '/home': (context) => const HomeScreen(),
+            },
+          );
+        },
+      ),
+    );
+  }
+}
